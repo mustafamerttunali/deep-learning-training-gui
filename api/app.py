92f18b675e586f2089e6337555222835709d08cc
Mustafa Mert Tunalı <tunalimustafamertt@gmail.com>
2019-12-17 23:34:59 +0300

    Add files via upload

---
+++ api/app.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+
+
+#model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/training')
+def training():
+    return render_template('training.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+   
